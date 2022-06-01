1cf04c01f659d8e916591a48fac28a3940cdffc4
Luis Enrique Carreño Herreño <luisenriquecarreno@hotmail.com>
2022-06-01 22:25:50 +0000

    Initial commit. Content taken from the Dash Basics class.

---
+++ app_solutions/lib/title.py
@@ -0,0 +1,17 @@
+# Basics Requirements
+import pathlib
+from dash import Dash, callback, html, dcc, dash_table, Input, Output, State, MATCH, ALL
+
+# Dash Bootstrap Components
+import dash_bootstrap_components as dbc
+
+# Recall app
+from app import app
+
+title = html.Div(
+    className="ds4a-title",
+    children=[
+        dbc.Row(dbc.Col(html.H1("US Sales Dashboard"), width={"size": 6, "offset": 3}))
+    ],
+    id="title",
+)
